8d57811e145be3f7bc85abf9c3beabad800b37a6
Valentin <valentinignaciopenna@gmail.com>
2025-03-13 16:43:25 -0300

    jwt

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -2,18 +2,20 @@ import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "../config/config";
 
-export const authMiddleware = (req: Request, res: Response, next: NextFunction) => {
+export const authMiddleware = (req: Request, res: Response, next: NextFunction): void => {
     const token = req.headers["authorization"]?.split(" ")[1]; // Obtener el token del encabezado
 
     if (!token) {
-        return res.status(403).json({ error: "Token no proporcionado" });
+        res.status(403).json({ error: "Token no proporcionado" });
+        return;
     }
 
     jwt.verify(token, JWT_SECRET, (err, decoded) => {
         if (err) {
-            return res.status(401).json({ error: "Token inválido" });
+            res.status(401).json({ error: "Token inválido" });
+            return;
         }
         req.userId = (decoded as any).id; // Almacenar el ID del usuario en la solicitud
-        next();
+        next(); // Llamar a next() para continuar con la siguiente función de middleware
     });
 }; 
